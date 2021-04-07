2a4b832a7f033f1ba51f89451e1d7d091427bb8c
AbdullahQureshi <adnx1080@gmail.com>
2021-04-07 20:02:24 +0500

    updated components

--- components/Modal/SelectUserModal.js
+++ components/Modal/SelectUserModal.js
@@ -9,68 +9,13 @@ import SearchBar from '../../components/SearchBar';
 // import { TouchableOpacity } from 'react-native-gesture-handler';
 const { width, height } = Dimensions.get("screen");
 
-// const ViewUsers =({data})=>{
-//     console.log("In view all users")
-//     console.log(data);
-//     return (
-//         <View>
-//             {
-//                 data.map((user)=>{
-
-//                     <View  key={user._id}>
-//                         <View style={{backgroundColor:"red"}}>
-//                             <Text>shkgfd</Text>
-//                         </View>
-//                         <Text>{user.name}</Text></View>
-//         })
-//             }
-//         </View>
-//     )
-//         }
-    // return data.map(function(user, i){
-    //     // const jsonData = JSON.stringify(user) ;
-     
-    //       <View key={i} style={{backgroundColor:"red",flex:1,}}>
-    //         <AppText>{user.name}</AppText>
-    //         {/* <View>
-    //           <Text>{news.text}</Text>
-    //         </View> */}
-    //       </View>
-    //   })
-//     return data.map((user)=>{
-
-//             <View  key={user._id}>
-//                 <View style={{backgroundColor:"red"}}>
-//                     <Text>shkgfd</Text>
-//                 </View>
-//                 <Text>{user.name}</Text></View>
-// }
-//     )}
-
-
 export default function SelectUserModal({onChangeText,btnName, btnCloseName, isVisible,onPressAdd, onPressCancel,handlePress,style,imageUri,data}) {
-    
-    // const ViewUsers =({data})=>{
-    //     console.log("In view all users")
-    //     console.log(data);
-    //     return data.map(function(user, i){
-    //         // const jsonData = JSON.stringify(user) ;
-         
-    //           <View key={i} style={{backgroundColor:"red",flex:1,}}>
-    //             <AppText>{user.name}</AppText>
-    //             {/* <View>
-    //               <Text>{news.text}</Text>
-    //             </View> */}
-    //           </View>
-    //       })
-    // }
     
     return (
         <Modal visible={isVisible} presentationStyle="formSheet">
         <View style={styles.modalView}>
         <View style={{ marginVertical: 10, flexDirection:"row" , justifyContent:"space-between" }}>
             <AppButton name={btnCloseName}onPress={onPressCancel} />
-            {/* <AppButton name={btnName} onPress={onPressAdd} /> */}
           </View>
           <View style={{ marginVertical: 10 }}>
               <SearchBar placeholder="Search user by email" onChangeText={onChangeText} />
@@ -79,34 +24,25 @@ export default function SelectUserModal({onChangeText,btnName, btnCloseName, isV
                data={data}
                keyExtractor={(user, index) => index.toString()}
                renderItem = {({item}) => (
-                <TouchableOpacity  onPress={()=>onPressAdd(item)} key={item._id} style={{backgroundColor:"rgba(73, 84, 100, 0.1)",flex:1, flexDirection:"row", marginVertical:8, borderRadius:10,}}>
-                {/* <View  key={item._id}style={{backgroundColor:"rgba(73, 84, 100, 0.1)",flex:1, flexDirection:"row", marginVertical:8, borderRadius:10,}}> */}
+                <View  key={item._id} style={{flexDirection:"row",alignItems: 'center',}} >
+                    <View style={{backgroundColor:"rgba(73, 84, 100, 0.1)",flex:1, flexDirection:"row", marginVertical:8, borderRadius:10,}}>
                     <Image source={{uri:item.image?item.image:"https://via.placeholder.com/150"}} style={{ marginLeft:5,height:60, width:60, borderRadius:60, alignSelf: 'center',}}/>
                     <View style={{marginHorizontal:5}}>
                            <AppText style={{fontSize:16}}>{item.name}</AppText>
                             <AppText style={{fontSize:14}}>{item.email}</AppText>
                             <AppText style={{fontSize:14}}>{item.jobcategory}</AppText>
                     </View>
+                    </View>
+                    <View style={{backgroundColor:"#1b262c", marginHorizontal:5, borderRadius:20,}}>
+                    <TouchableOpacity onPress={()=>onPressAdd(item)} >
+                    <MaterialIcons name="navigate-next" size={30} color="#e8e8e8" />
+                    </TouchableOpacity>
+                    </View>
                 {/* </View> */}
-                </TouchableOpacity>
+                </View>
       )}
       />
-          {/* <TouchableWithoutFeedback onPress={handlePress}>
-      <View style={{...styles.container, ...style}}>
-        {!imageUri && (
-          <MaterialIcons color="white" name="add" size={40} />
-        )}
-        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-      </View>
-    </TouchableWithoutFeedback> */}
           </View>
-          {/* {
-              data.map((user)=>{
-                return(
-                    <View id={user._id}><Text>{user}</Text></View>
-                )
-            })
-          } */}
         </View>
       </Modal>
     )
--- components/ProfessionalAvatar.js
+++ components/ProfessionalAvatar.js
@@ -3,17 +3,18 @@ import * as React from 'react';
 import { Avatar } from 'react-native-paper';
 import {View,Text,TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AppText from './AppText';
 
 
 const ProfessionalAvatar = (props) => {
  return(
      <TouchableOpacity onPress={props.onPress} disabled={props.disabled}>
         <View style={props.style}>
-            {!props.imageUri?<MaterialCommunityIcons color="#1B262C" name="camera" size={40} />
+            {!props.imageUri?<MaterialCommunityIcons color="#1B262C" name="camera" size={35} />
             : <Avatar.Image size={props.size} source={{uri:props.imageUri}} />}
-             <Text style = {props.style.nameText}>{props.name}</Text>
-             <Text style = {props.style.titleText}>{!props.title?"Job Title Not Set":props.title}</Text>
-             <Text style = {props.style.titleText}>{props.email}</Text>
+             <AppText style = {props.style.nameText}>{props.name}</AppText>
+             <AppText style = {props.style.titleText}>{!props.title?props.placeholdertext:props.title}</AppText>
+             <AppText style = {props.style.titleText}>{props.email}</AppText>
         </View> 
     </TouchableOpacity>
     
