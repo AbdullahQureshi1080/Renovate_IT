dae9fc42bfc47814165bcb2818fa9705b3a45d70
AbdullahQureshi <adnx1080@gmail.com>
2021-04-10 01:47:25 +0500

    updated navigation-stack remote-firm

--- navigation/ProfileStack.js
+++ navigation/ProfileStack.js
@@ -2,13 +2,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View,TouchableOpacity,StyleSheet} from 'react-native';
-import AppButton from '../components/AppButton'
+// import { View,TouchableOpacity,StyleSheet} from 'react-native';
+// import AppButton from '../components/AppButton'
 
 // Screen Imports
 import UserProfileScreen from '../screens/ProfileStack/UserProfileScreen';
 import UpdateProfileScreen from '../screens/ProfileStack/UpdateProfileScreen';
-import { Button } from 'react-native-paper';
+// import { Button } from 'react-native-paper';
+import FirmManageScreen from '../screens/ProfileStack/FirmManageScreen';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,9 @@ const ProfileStack = () => {
                 headerShown:false,
             }} />
             <Stack.Screen name="Edit Profile" component={UpdateProfileScreen} options={{
+                headerShown:false,
+            }} />
+            <Stack.Screen name="Firm Details" component={FirmManageScreen} options={{
                 headerShown:false,
             }} />
         </Stack.Navigator>
