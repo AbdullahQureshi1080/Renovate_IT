e4d86a407ce60f47fdcd76d613a319e7749c4879
AbdullahQureshi <adnx1080@gmail.com>
2021-04-15 20:56:34 +0500

    switching client heroku-api

--- api/client.js
+++ api/client.js
@@ -2,8 +2,8 @@ import {create} from 'apisauce';
 // import { response } from 'express';
 
 const client = create({
-    baseURL: "http://192.168.18.106:9000/api/",
-    // baseURL: "https://renovateit.herokuapp.com/api/",
+    // baseURL: "http://192.168.18.106:9000/api/",
+    baseURL: "https://renovateit.herokuapp.com/api/",
 })
 
 
