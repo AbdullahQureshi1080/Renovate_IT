99811dfca2a16afdca394e5ed45ef18828f73b67
Abdullah Qureshi <adnx1080@gmail.com>
2021-05-22 15:06:56 +0500

    changing client to heroku

--- api/client.js
+++ api/client.js
@@ -2,8 +2,8 @@ import {create} from 'apisauce';
 // import { response } from 'express';
 
 const client = create({
-  baseURL: 'http://192.168.18.13:9000/api/',
-  // baseURL: "https://renovateit.herokuapp.com/api/",
+  // baseURL: 'http://192.168.18.13:9000/api/',
+  baseURL: 'https://renovateit.herokuapp.com/api/',
 });
 
 export default client;
